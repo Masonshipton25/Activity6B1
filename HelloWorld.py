b5ee9c4d2f39fcf98f223e5fed5f3d3a3ac73f68
Mason <mason.shipton@ontariotechu.net>
2025-02-13 20:20:01 -0500

    Add HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,17 @@
+# HelloWorld.py
+
+# Programmer: Mason Shipton
+# Description: An elaborate way to print "Hello World" to the terminal
+# Date Created: 2025-02-13
+# Date Last Revised: 2025-02-13
+
+#Returns "Hello"
+def printHello():
+    return "Hlelo"
+
+#Returns "World"
+def printWorld():
+    return "World"
+
+#Prints "Hello World" to the terminal
+print(printHello() + " " + printWorld())
